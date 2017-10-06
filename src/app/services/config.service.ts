e4641ba2c89285dafce4bdb4708134f4931a8065
Jared Eiseman <jared.eiseman@aceindustries.com>
2017-10-05 20:15:21 -0700

    prep for deploy

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -4,8 +4,8 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ConfigService {
 
-  // baseURI: string = '/api';
-  baseURI: string = 'http://localhost:3000/api';
+  baseURI: string = '/api';
+  // baseURI: string = 'http://localhost:3000/api';
 
   constructor(private http: Http) { }
 
